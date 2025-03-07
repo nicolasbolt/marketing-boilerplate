e55e791f9ad5820ecd6bb2097f9c7c7ea56cbdf8
Nicolas Bolt <nicolas.bolt@gmail.com>
2025-03-07 09:20:03 -0500

    update testimonials page

--- components/landing/Testimonials.jsx
+++ components/landing/Testimonials.jsx
@@ -11,35 +11,35 @@ import {
 const testimonials = [
   {
     image: 'https://github.com/shadcn.png',
-    name: 'John Doe React',
+    name: 'John Doe',
     userName: '@john_Doe1',
     comment:
       'Lorem ipsum dolor sit amet,empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
   },
   {
     image: 'https://github.com/shadcn.png',
-    name: 'John Doe React',
+    name: 'John Doe',
     userName: '@john_Doe2',
     comment:
       'Lorem ipsum dolor sit amet,exercitation. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
   },
   {
     image: 'https://github.com/shadcn.png',
-    name: 'John Doe React',
+    name: 'John Doe',
     userName: '@john_Doe3',
     comment:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
   },
   {
     image: 'https://github.com/shadcn.png',
-    name: 'John Doe React',
+    name: 'John Doe',
     userName: '@john_Doe4',
     comment:
       'Lorem ipsum dolor sit amet, tempor incididunt  aliqua. Ut enim ad minim veniam, quis nostrud.',
   },
   {
     image: 'https://github.com/shadcn.png',
-    name: 'John Doe React',
+    name: 'John Doe',
     userName: '@john_Doe5',
     comment:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
